98610f02510f2d86a96ec42b96d9b1ebee5ae8b4
Kareem4874 <karemebrahim484@gmail.com>
2025-09-17 22:25:46 +0300

    Update code

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const token = request.cookies.get("next-auth.session-token") || request.cookies.get("__Secure-next-auth.session-token") || request.cookies.get("_vercel_jwt")
+    const token = request.cookies.get("__Secure-next-auth.session-token")?.value
     if(!token){
         return NextResponse.redirect(new URL('/Login', request.url))
     }
